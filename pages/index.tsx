c0996616747c89306409022773e714240e22a39b
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2019-10-30 00:54:37 +0900

    feat: not support browser error message for IE

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import GHCorner from 'react-gh-corner'
+import { detect } from 'detect-browser'
 
 const DynamicComponent = dynamic(
   () => import('../components/Photo'),
@@ -18,6 +19,7 @@ const Home = () => (
     <Head>
       <title>Pretty Text Generator</title>
       <link rel='icon' href='/favicon.ico' />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
       <meta property="og:site_name" content="Pretty Text Generator"/>
       <meta property="og:title" content="Create images of pretty letters!"/>
       <meta property="og:image" content="https://repository-images.githubusercontent.com/217741173/15ca9080-f9e3-11e9-94ef-4aa7a1f08a46"/>
@@ -34,7 +36,8 @@ const Home = () => (
 
     <div className='main'>
       <div className='title'>Pretty Text Generator</div>
-      <DynamicComponent />
+      { detect().name === 'ie' && <div>Not Support Browser. Please use <b>Google Chrome</b> browser!</div> }
+      { detect().name !== 'ie' && <DynamicComponent />}
     </div>
 
     <style jsx global>{`
